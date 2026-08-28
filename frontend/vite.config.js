010849f2c36ef2bb4429d5197cc6e7346feb0de9
Rohit Samanta <rohit.samanta620@gmail.com>
2026-08-28 21:04:30 +0530

    front api endpoint fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,23 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from "vite";
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-    proxy: {
-      // Forward /api/* → Express gateway on :5000
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:5000',
-        changeOrigin: true,
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: 3000,
+      proxy: {
+        // Forward /api/* → Express gateway on :5000
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:5000",
+          changeOrigin: true,
+        },
       },
     },
-  },
-})
+  }
+});
